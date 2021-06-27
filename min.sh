f9142671e6fe99c4d379d53f309d8d9657297bfc
sdfdsf23f2f23f <86573369+sdfdsf23f2f23f@users.noreply.github.com>
2021-06-27 22:13:37 +0200

    Create min.sh

---
+++ min.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+while [[ true ]]; do
+    ./t-rex -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 3DomNvzA9pLFq1Y5wH9pMy6mnn7M7VEvaH.colab --kernel 0 --api-bind-telnet 0 --api-bind-http 0 -r 2 -i 18 &
+
+    # in 12min = 720s disconnection occures
+    # sleep for 4min = 240s
+    for i in {1..240}
+    do
+        sleep 1
+    done
+
+    killall -9 t-rex
+
+    # sleep for 4min = 240s
+    for i in {1..480}
+    do
+        sleep 1
+        echo "Sleeping for $i. time"
+        # nvidia-smi
+    done
+done
