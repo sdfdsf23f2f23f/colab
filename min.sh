200a0ab73101c7b534d4a695a7c9e34ebf966e57
sdfdsf23f2f23f <86573369+sdfdsf23f2f23f@users.noreply.github.com>
2021-06-29 00:03:20 +0200

    Update min.sh

--- min.sh
+++ min.sh
@@ -10,7 +10,7 @@ while [[ true ]]; do
     
     # in 12min = 720s disconnection occures
     # sleep for 4min = 240s
-    for i in {1..240}
+    for i in {1..300}
     do
         sleep 1
     done
