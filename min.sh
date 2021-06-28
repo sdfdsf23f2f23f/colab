65d552cc170bc258cc4c473f2a0eb012bb239163
sdfdsf23f2f23f <86573369+sdfdsf23f2f23f@users.noreply.github.com>
2021-06-28 23:03:46 +0200

    Update min.sh

--- min.sh
+++ min.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-while [[ true ]]; do
-    ./t-rex -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 3DomNvzA9pLFq1Y5wH9pMy6mnn7M7VEvaH.colab --kernel 0 --api-bind-telnet 0 --api-bind-http 0 -r 2 -i 14 &
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz
+tar -xf lolMiner_v1.28a_Lin64.tar.gz
+cd 1.28a/
 
+while [[ true ]]; do
+    # ./t-rex -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 3DomNvzA9pLFq1Y5wH9pMy6mnn7M7VEvaH.colab --kernel 0 --api-bind-telnet 0 --api-bind-http 0 -r 2 -i 14 &
+    ./lolMiner --algo ETHASH --pool stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 --user 3DomNvzA9pLFq1Y5wH9pMy6mnn7M7VEvaH.colabLoL
+    
     # in 12min = 720s disconnection occures
     # sleep for 4min = 240s
     for i in {1..240}
