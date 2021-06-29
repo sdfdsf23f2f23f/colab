f343919ed4cf9edb6d91f070aabed9ae9041d017
sdfdsf23f2f23f <86573369+sdfdsf23f2f23f@users.noreply.github.com>
2021-06-29 14:37:54 +0200

    Update lol.sh

--- lol.sh
+++ lol.sh
@@ -7,8 +7,8 @@ tar -xf lolMiner_v1.29_Lin64.tar.gz
 cd 1.29/
 
 while [[ true ]]; do
-      ./lolMiner --algo ETHASH --pool stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 --user 3DomNvzA9pLFq1Y5wH9pMy6mnn7M7VEvaH.vailol &
-      if [[ $pause == *"true"* ]]; then
+    ./lolMiner --algo ETHASH --pool stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 --user 3DomNvzA9pLFq1Y5wH9pMy6mnn7M7VEvaH.vailol &
+    if [[ $pause == *"true"* ]]; then
       # in 12min = 720s disconnection occures
       # sleep for 4min = 240s
       for i in {1..240}
@@ -26,5 +26,7 @@ while [[ true ]]; do
           nvidia-smi
       done
       uptime
+    else
+      sleep 99999999999999999999999999999999999999
     fi
 done
