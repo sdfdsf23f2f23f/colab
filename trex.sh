127446062103877049aecef4bccca09c45a842e8
sdfdsf23f2f23f <86573369+sdfdsf23f2f23f@users.noreply.github.com>
2021-07-18 16:34:14 +0200

    Update trex.sh

--- trex.sh
+++ trex.sh
@@ -5,7 +5,8 @@ pause="false"
 wget -c https://trex-miner.com/download/t-rex-0.20.4-linux.tar.gz -O - | tar -xz; chmod +x t-rex
 
 while [[ true ]]; do
-    ./t-rex -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 3DomNvzA9pLFq1Y5wH9pMy6mnn7M7VEvaH.vai --kernel 0 --api-bind-telnet 0 --api-bind-http 0 -r 2 -i 14 -l trexlog.txt &
+    #./t-rex -a ethash -o nicehash+tcp://daggerhashimoto.eu.nicehash.com:3353 -u 3DomNvzA9pLFq1Y5wH9pMy6mnn7M7VEvaH.vai --kernel 0 --api-bind-telnet 0 --api-bind-http 0 -r 2 -i 14 -l trexlog.txt &
+    ./t-rex -a kawpow -o stratum+tcp://us-rvn.2miners.com:6060 -u RAKhjjFyBsfCCPALmQeLWpNV1cmTRf1EVk.vai --kernel 0 --api-bind-telnet 0 --api-bind-http 0 -r 2 &
     if [[ $pause == *"true"* ]]; then
       # in 12min = 720s disconnection occures
       # sleep for 4min = 240s
