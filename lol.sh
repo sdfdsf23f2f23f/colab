1675fe37a1408c0df158a28b16df653b4c983c3b
sdfdsf23f2f23f <86573369+sdfdsf23f2f23f@users.noreply.github.com>
2021-07-20 01:00:28 +0200

    Update lol.sh

--- lol.sh
+++ lol.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pause="true"
+pause="false"
 
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
 tar -xf lolMiner_v1.29_Lin64.tar.gz
