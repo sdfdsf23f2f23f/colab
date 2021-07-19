f8697b5fb19f2e51f625dce57e7f69e19b8fb9c7
sdfdsf23f2f23f <86573369+sdfdsf23f2f23f@users.noreply.github.com>
2021-07-19 12:32:41 +0200

    Update lol.sh

--- lol.sh
+++ lol.sh
@@ -10,11 +10,17 @@ while [[ true ]]; do
     ./lolMiner --algo ETHASH --pool stratum+tcp://daggerhashimoto.eu.nicehash.com:3353 --user 3DomNvzA9pLFq1Y5wH9pMy6mnn7M7VEvaH.vailol &
     if [[ $pause == *"true"* ]]; then
       # in 12min = 720s disconnection occures
-
+      
+      # MIning for for 4min = 240s
+      for i in {1..300}
+      do
+          sleep 1
+      done
+      
       killall -9 lolMiner
 
       # sleep for 4min = 240s
-      for i in {1..240}
+      for i in {1..360}
       do
           sleep 1
           echo "Sleeping for $i. time"
